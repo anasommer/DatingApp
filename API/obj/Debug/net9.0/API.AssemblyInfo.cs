db94054040353e30b9f7371a4c70ce26e284659c
Anastassia Sommer <anastassia.sommer@gmail.com>
2025-07-10 23:04:55 +0200

    Install tailwind, daisyui, certs for https

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d92ffc940bd5480c27528e617b7c392862c45f9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d0bd89f834cca6a6dda8a57b488e6acfc455c36")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
