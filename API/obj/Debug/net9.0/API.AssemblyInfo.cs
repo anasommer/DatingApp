a1bc2d4e97228b03649f519c0b96d11519c84f4d
Anastassia Sommer <anastassia.sommer@gmail.com>
2025-07-11 22:19:04 +0200

    create account controller

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d0bd89f834cca6a6dda8a57b488e6acfc455c36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db94054040353e30b9f7371a4c70ce26e284659c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
