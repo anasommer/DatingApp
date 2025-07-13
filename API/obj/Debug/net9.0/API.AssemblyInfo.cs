ab2f829514a357ac9cb5fee6cb6cf40d08bcfb81
Anastassia Sommer <anastassia.sommer@gmail.com>
2025-07-13 10:08:39 +0200

    add nav component

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0100d82fd7393bc51b43af39190810d79d2bb64f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd567dc3b6d904d60831676d60f341dafe7c01fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
